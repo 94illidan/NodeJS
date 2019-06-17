477416e72ad3221b4231fb88ecd93362d076a9e2
94illidan <51425276+94illidan@users.noreply.github.com>
2019-06-17 20:18:42 -0300

    creacion de un nuevo programa, ejercicios de hackerrank

---
+++ futuro_programa.js
@@ -0,0 +1,12 @@
+let edad = 20;
+let permiso_para_manejar = ["no tiene edad para manejar", "Tiene edad para manejar"];
+let nombre = prompt = ("di un nombre");
+
+if (edad < 18) {
+    permiso = permiso_para_manejar [0];
+    }else{
+    permiso = permiso_para_manejar [1];
+}
+
+console.log (permiso);
+console.log (nombre);
