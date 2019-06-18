75462f66882fc00c3a01f0b669155c1d75281034
Matias Fernandez <L0628654@bancogalicia.com.ar>
2019-06-18 17:21:55 -0300

    realizamos el primer ejercicio de hackerrank

--- hackerrank.js
+++ hackerrank.js
@@ -1,9 +1,9 @@
-let nombre = "Welcome to 30 Days of Code!"
 function processData(inputString) {
     // This line of code prints the first line of output
-    console.log(nombre);
     console.log("Hello, World.");
+    console.log(inputString);
 
     // Write the second line of output that prints the contents of 'inputString' here.
+}
 
-} 
+processData ("Welcome to 30 Days of Code!")
