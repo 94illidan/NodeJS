d6b710b2edd30a2b4fc9c3f4baf5900baae7a10d
Matias Fernandez <L0628654@bancogalicia.com.ar>
2019-06-18 17:52:37 -0300

    2da parte del curso hackerrank

--- hackerrank.js
+++ hackerrank.js
@@ -1,3 +1,4 @@
+/*
 function processData(inputString) {
     // This line of code prints the first line of output
     console.log("Hello, World.");
@@ -7,3 +8,18 @@ function processData(inputString) {
 }
 
 processData ("Welcome to 30 Days of Code!")
+*/
+
+//-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*//
+
+let i = 12
+let d = 4.0
+let s = "the best place to learn and practice coding!"
+
+
+let int = (i + i);
+let dobuble = (d + d);
+let strign = (i + d);
+
+
+console.log (int, dobuble, strign);
