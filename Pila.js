3460980982d1f4ccc018c718a63ca432b31db729
94illidan <51425276+94illidan@users.noreply.github.com>
2019-06-23 16:49:58 -0300

    pilas ejercicio

---
+++ Pila.js
@@ -0,0 +1,20 @@
+class stack{
+    constructor (){
+
+    }
+    Pila1(){
+            console.log ("numero " + pilas.dato1);
+        }
+    }
+
+
+
+
+let pilas = new stack();
+pilas.dato1 = 1
+pilas.dato2 = 2
+pilas.dato3 = 3
+pilas.dato4 = 4
+pilas.dato5 = 5
+pilas.Pila1();
+
