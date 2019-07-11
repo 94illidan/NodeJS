a3c39b1b5841d05eee6ba151ca144ad915dac63d
Matias Fernandez <L0628654@bancogalicia.com.ar>
2019-07-11 17:22:26 -0300

    mod de archivo prueba para realizar un programa para ingresar datos desde el teclado

--- Prueba.js
+++ Prueba.js
@@ -1,17 +1,48 @@
-class Persona{
-    constructor(nombre, Apellido){
-        this.nombre = nombre;
-        this.Apellido = Apellido
-    }
-    Saludo(){
-        console.log("hola, mi nombre es " + this.nombre + " y mi apellido es " + this.Apellido);
-    }
-}
+// class Persona{
+//     constructor(nombre, Apellido){
+//         this.nombre = nombre;
+//         this.Apellido = Apellido
+//     }
+//     Saludo(){
+//         console.log("hola, mi nombre es " + this.nombre + " y mi apellido es " + this.Apellido);
+//     }
+// }
 
-let persona = new Persona("Matias", "Fernandez");
-persona.nombre2 = ("Matias", "Puricceli");
+// let persona = new Persona("Matias", "Fernandez");
+// persona.nombre2 = ("Matias", "Puricceli");
 
-persona.Saludo() + persona.nombre2()
+// persona.Saludo() + persona.nombre2()
 
 
 
+
+
+
+
+
+// console.log("Escribe tu nombre");
+// var stdin = process.openStdin();
+ 
+// stdin.addListener("data", function(d) {console.log("Tu nombre es: " + d.toString().trim());});
+
+
+// process.stdin.on ("data", function(data){
+//     console.log("Tu nombre es " + data.toString())
+// })
+
+
+console.writeLine("hi")
+
+
+
+// function nombre(data){
+//     data();
+// }
+
+// var datos = function(){
+//     console.log ("bye");
+// }
+
+
+
+// nombre(datos);
